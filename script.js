558d1fec0aae9a119dd9fbd57ab56ed15dcd5c46
jonn <joliversalamanca@gmail.com>
2025-12-04 17:17:47 -0400

    se modifico el envio de datos del formulario

--- script.js
+++ script.js
@@ -18,8 +18,7 @@
 
     })
 
-})
-    (document);
+})(document);
 
 /* >>>>>>>>>>>>>>>=>>>CONTACT FORM<<<<<<<<<<<<<<<<<<<<<<< */
 ((d) => {
@@ -30,36 +29,25 @@
     $contactForm.addEventListener("submit", (e) => {
         e.preventDefault();
         $contactLouder.classList.remove("none");
-
-        const formData = new FormData(e.target);
-        const data = {
-            access_key: "d2c92fb7-b0a8-4ba7-8b7a-e5d8f3c9a2b1",
-            name: formData.get("name"),
-            email: formData.get("email"),
-            comments: formData.get("comments")
-        };
-
-        fetch("https://api.web3forms.com/submit", {
+        fetch("https://formsubmit.co/ajax/joliversalamanca@gmail.com", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
+            body: new FormData(e.target)
         })
             .then((res) => (res.ok ? res.json() : Promise.reject(res)))
             .then(json => {
                 console.log(json);
                 location.hash = "#gracias";
                 $contactForm.reset();
+
             })
             .catch(error => {
-                console.log("Error:", error);
-                let message = "Ocurrió un error, vuelva a intentarlo";
-                $contacResponse.querySelector("h3").innerHTML = `Error: ${message}`;
+                console.log(error);
+                let message = error.status || "Ocurrio un error vuleva a intentarlo";
+                $contacResponse.querySelector("h3").innerHTML = `Error ${error.status}:${message}`;
             })
             .finally(() => {
                 $contactLouder.classList.add("none");
-                setTimeout(() => {
+                setInterval(() => {
                     location.hash = "#close";
                 }, 2000);
             });
